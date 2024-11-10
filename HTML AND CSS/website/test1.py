ba70b0da0dc0be7f4700f8d048f5a148d21b5930
ryemint <mosinabre.paulabianca@gmail.com>
2024-11-10 20:54:46 +0800

    Add files via upload

---
+++ HTML AND CSS/website/test1.py
@@ -0,0 +1,22 @@
+#first
+"""username = input("Enter username: ")
+print("Username is: " + username)
+
+#2
+num1 = input("Enter a number: ")
+print("The number is: " + num1)
+
+num = int(input("Enter a number: "))
+print("Number: ", num + 1)
+print(type(num))
+
+#4
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter the second number: "))
+print("answer: ", num1 // num2)
+
+#5
+num = float(input("Enter first number: "))
+print("{:.2f}.format(num)")"""
+
+#6
