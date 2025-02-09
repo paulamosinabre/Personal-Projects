4d28f80bda51ee20951a8ef8fe7bb84d23692950
Paula Bianca Mosinabre <mosinabre.paulabianca@gmail.com>
2025-02-09 19:02:51 +0800

    RockPaperScissors.java

--- JAVA/RockPaperScissors/RockPaperScissors.java
+++ JAVA/RockPaperScissors/RockPaperScissors.java
@@ -4,6 +4,7 @@ package com.mycompany.rockpaperscissors;
 public class RockPaperScissors {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        FrmStartPage start = new FrmStartPage();
+        start.setVisible(true);
     }
 }
