95ba74d9f5cac2b0406f3c16973e24fd67629596
Paula Bianca Mosinabre <mosinabre.paulabianca@gmail.com>
2025-03-13 22:36:19 +0800

    GuessTheNumber.java

---
+++ JAVA/GuessTheNumber/GuessTheNumber.java
@@ -0,0 +1,12 @@
+package com.mycompany.guessthenumber;
+
+
+public class GuessTheNumber {
+
+    public static void main(String[] args) {
+        MyFrame frm = new MyFrame();
+        
+        frm.setVisible(true);
+        frm.setLocationRelativeTo(null);
+    }
+}
